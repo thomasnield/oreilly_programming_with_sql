8d671bfabac9f5c735b4255a6af171ac453d68fd
Thomas Nield <thomasnield@live.com>
2022-06-01 09:20:23 -0500

    Update 4_1_pooling.java

--- code/section_iv/4_1_pooling.java
+++ code/section_iv/4_1_pooling.java
@@ -12,7 +12,9 @@ public class JavaLauncher {
 
         try {
             HikariConfig config = new HikariConfig();
-            config.setJdbcUrl("jdbc:sqlite://C:/git/oreilly_programming_with_sql/thunderbird_manufacturing.db");
+            //config.setJdbcUrl("jdbc:sqlite://C:/git/oreilly_programming_with_sql/thunderbird_manufacturing.db");
+            config.setJdbcUrl("jdbc:sqlite:///Users/thomasnield/git/oreilly_intermediate_sql_for_data/thunderbird_manufacturing.db");
+
             config.setMinimumIdle(1);
             config.setMaximumPoolSize(5);
 
